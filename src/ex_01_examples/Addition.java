4baddb8c72c41c92b59bf424ccc7b25308fefbd5
Gunda Aishwarya <gundaaishwarya@gmail.com>
2025-01-18 04:15:08 +0530

    Add example

---
+++ src/ex_01_examples/Addition.java
@@ -0,0 +1,9 @@
+package ex_01_examples;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        System.out.println(a+b);
+    }
+}
