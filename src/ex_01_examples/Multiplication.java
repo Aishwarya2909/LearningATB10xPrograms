b2af2796d3e66f328c996ce97bd90e3e8ff7642b
Gunda Aishwarya <gundaaishwarya@gmail.com>
2025-01-18 04:23:58 +0530

    Multiplication example

---
+++ src/ex_01_examples/Multiplication.java
@@ -0,0 +1,9 @@
+package ex_01_examples;
+
+public class Multiplication {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+        System.out.println(a*b);
+    }
+}
