0486b07a38d18337f264d63969deb7c976baa005
Gunda Aishwarya <gundaaishwarya@gmail.com>
2025-01-18 06:05:48 +0530

    Operators example

---
+++ src/ex_01_examples/Operators.java
@@ -0,0 +1,13 @@
+package ex_01_examples;
+
+public class Operators {
+    public static void main(String[] args) {
+        int a =10;
+        int b = 20;
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a==b);
+        System.out.println((a*b)/100+20-10);
+
+    }
+}
