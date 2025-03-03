e44eed4c7ff3837c1e331d5e2831ba78787b4793
Gunda Aishwarya <gundaaishwarya@gmail.com>
2025-03-04 01:14:19 +0530

    tasks_of_9thfeb

---
+++ src/ex_9thfeb_tasks/SecondSmallestINumInArray.java
@@ -0,0 +1,34 @@
+package ex_9thfeb_tasks;
+
+public class SecondSmallestINumInArray {
+
+    public static void main(String[] args) {
+
+        int[] numbers = {13, 6, 56, 1, 34, 67, 89};
+
+        int smallest = Integer.MAX_VALUE;
+        int second_smallest = Integer.MAX_VALUE;
+//        System.out.println(smallest);
+//        System.out.println(second_smallest);
+
+        for(int num : numbers)
+        {
+            if (num<smallest )
+            {
+                second_smallest = smallest;   // update old smallest to second_smallest
+                smallest = num;
+            } else if (num>smallest && num < second_smallest)
+            {
+                second_smallest = num;
+            }
+        }
+
+        if (second_smallest == Integer.MAX_VALUE) {
+            System.out.println("No second smallest element found.");
+        } else {
+            System.out.println("Second smallest number: " + second_smallest);
+        }
+
+        }
+    }
+
---
+++ src/ex_9thfeb_tasks/SortTheArray.java
@@ -0,0 +1,36 @@
+package ex_9thfeb_tasks;
+
+import java.util.Arrays;
+
+import static java.lang.String.*;
+
+public class SortTheArray {
+
+    public static void main(String[] args) {
+
+/*  Sort the array → int[] numbers = {12, 34, 10, 1, 100, 3, 4, 32};
+
+1,3,4,10,12,32,34,100
+
+without using any functions.
+
+Hint - compare and replace(temp) */
+
+        int[] numbers = {12, 34, 10, 1, 100, 3, 4, 32};
+        int n = numbers.length-1;
+
+          for(int i=0;i<n;i++) {
+            for (int j = 0; j < n - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        for(int num : numbers)
+        {
+            System.out.print(num + " ");
+        }
+    }
+}
